fa5164d163d24c97f51e11e8db4e315d7185c407
DaisyWM <daisy.muiruri@strathmore.edu>
2024-11-23 11:36:07 +0300

    refactor: Updated SplashActivity.kt

--- app/src/main/java/com/example/carrentalapp/SplashActivity.kt
+++ app/src/main/java/com/example/carrentalapp/SplashActivity.kt
@@ -6,14 +6,19 @@ import android.os.Bundle
 import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import com.example.carrentalapp.ui.theme.CarRentalAppTheme
@@ -33,14 +38,23 @@ class SplashActivity: ComponentActivity() {
 
     @Composable
     private fun SplashScreen(){
+        val alpha = remember {
+            Animatable(0f)
+        }
         LaunchedEffect(key1 = true, block = {
+            alpha.animateTo(1f,
+            animationSpec = tween(1500)
+
+            )
             delay(2000)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
         })
         Box(modifier = Modifier.fillMaxSize().background(
             Color(0xFFFFFFFF)
         )  , contentAlignment = Alignment.Center){
-            Image(painter = painterResource(R.drawable.suv_jpeg), contentDescription = null)
+            Image(
+                modifier = Modifier.alpha(alpha.value),
+                painter = painterResource(R.drawable.suv_jpeg), contentDescription = null)
         }
     }
 }
